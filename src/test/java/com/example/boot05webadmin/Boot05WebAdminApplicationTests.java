ca9274f42d0dee271c84ade1d4e06b6cf4829a44
swain <11@qq>
2022-05-04 22:15:10 +0800

    first commit

---
+++ src/test/java/com/example/boot05webadmin/Boot05WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot05webadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05WebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
